6f04f5dacddc8b9fc0e36aebcdd41c7d1c3ee641
limhaekyu <limk9500@gmail.com>
2022-09-26 17:24:15 +0900

    Initial commit

---
+++ src/main/java/com/example/sms_authentication_test/SmsAuthenticationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sms_authentication_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAuthenticationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsAuthenticationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sms_authentication_test/SmsAuthenticationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sms_authentication_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsAuthenticationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
